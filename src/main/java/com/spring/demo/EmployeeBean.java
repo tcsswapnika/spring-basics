70155bd5fedea1583c77f28c961fa1efa9ef0103
tcsswapnika <tcsswapnika@gmail.com>
2021-08-24 05:52:27 +0000

    first commit

---
+++ src/main/java/com/spring/demo/EmployeeBean.java
@@ -0,0 +1,23 @@
+package com.spring.demo;
+
+public class EmployeeBean {
+	private String fullName;
+	private DepartmentBean departmentBean;
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public DepartmentBean getDepartmentBean() {
+		return departmentBean;
+	}
+
+	public void setDepartmentBean(DepartmentBean departmentBean) {
+		this.departmentBean = departmentBean;
+	}
+
+}
---
+++ src/main/java/com/spring/demo/TestAutowire.java
@@ -0,0 +1,17 @@
+package com.spring.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestAutowire {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "application-context.xml" });
+
+		EmployeeBean employee = (EmployeeBean) context.getBean("employee");
+
+		System.out.println(employee.getFullName());
+
+		System.out.println(employee.getDepartmentBean().getName());
+	}
+
+}
